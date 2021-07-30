d153afc0b50ff403ab43dc6affd78c72ce921622
Rodrigo Milanez <rodrigomohland@hotmail.com>
2021-07-30 11:58:43 -0300

    thats it folks

--- js/controllers/PTCntrl.js
+++ js/controllers/PTCntrl.js
@@ -1,3 +1,9 @@
-angular.module("projetoTecnico").controller("projetoTecnicoControle", function ($scope) {
+angular.module("projetoTecnico").controller("projetoTecnicoControle", function ($scope, $http) {
     $scope.message = "Hello World!";
+    $scope.funcionarios = [];
+    var carregarFuncionarios = function(){
+        $http.get("http://localhost:8080/funcionarios").success(function(data, status){
+            console.log(data);
+        });
+    }
 });
