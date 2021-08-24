b7c80315fb29487774172a37f9a448d0cf144f95
Rodrigo Milanez <rodrigomohland@hotmail.com>
2021-08-24 10:20:23 -0300

    Post para imagens implementado

--- js/controllers/ordensDetalhesCtrl.js
+++ js/controllers/ordensDetalhesCtrl.js
@@ -15,6 +15,9 @@ $scope.deleteOrdem = function (ordemId) {
 };
     $scope.dlt=false;
 
+    $scope.uploadImagem=false;
+
+
     $scope.deleteEquipamento = function (ordemId, equipamentoId) {
         ordensService.deleteEq(ordemId ,equipamentoId).then(function (response) {
             window.location.reload()
@@ -29,54 +32,19 @@ $scope.deleteOrdem = function (ordemId) {
 
     //TERMINAR IMAGEM
     $scope.enviarFoto = function(ordens, id) {   
-        if(ordens.equipamentos.length > 1){
-
-            
-        }else{
-        var img = document.getElementById("file").files[0];
         var ordemId = ordens.id;
+        var img = document.getElementById("file").files[0];
         var equipamentoId = ordens.equipamentos.id;
         ordensService.imagemEq(ordemId, id, img).then(function (response) {
             console.log(img)
+            window.location.reload()
         }).catch(function(error){
             alert(error.data.message);
         })
     }   
-     };
-
-
-     /**
-      * $scope.uploadFiles = function(IT){
-        if(IT.length>1){
-            var verifica = true;
-            for(var i=0;i<IT.length;i++) {
-                if(file[i].files[0] == null){
-                    console.log(verifica)
-                }
-            }
-            if(verifica == true) {
-                for(var i=0;i<IT.length;i++) {
-                   var equipamentoID = IT[i].equipamento.id;    
-                    ordemServicoService.adicionarImagem(equipamentoID,
-                                                            file[i].files[0]).then((response) => {
-                        $scope.uploadIMG = true;
-                    }).catch(function(error){
-                        alert(error.data.message);
-                    })
-                }
-            }
-        }else{
-            var img = document.getElementById("file").files[0];
-            var IdEquipamento = IT[0].equipamento.id;  
-            ordemServicoService.adicionarImagem(IdEquipamento, img).then((response) => {
-                console.log(response)
-                $scope.uploadIMG = true;
-            }).catch(function(error){
-                alert(error.data.message);
-            })
-        }
     }
 
-      */
+    
 
-})
+
+)
